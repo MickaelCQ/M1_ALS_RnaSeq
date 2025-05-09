1727d557d31db37a90f778bd66e24a5b0050cb47
Mickael Coquerelle <mickael.coquerelle@etu.umontpellier.fr>
2025-05-09 17:51:44 +0200

    Add script to generate DevOps Log File

---
+++ 3.Home_script/Memo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+clear
+echo "###########################################################################|     |#####################################################|" 
+echo "                          Raccourcis Tmux                                 #|     |#           Raccourcis Visual Data  (vd)            #|"
+echo "###########################################################################|     |#####################################################|"
+echo "                                                                          #|     |#                                                   #|"
+echo "ctrl + a     | c            : Ouvrir une nouvelle fenêtre                 #|     |# g/G     : Aller en haut ou en bas du fichier      #|"
+echo "ctrl + a     | ,            : Renommer une fenetre courante               #|     |# TAB     : Pour changer de feuille si plusieurs    #|"
+echo "ctrl + a     | x            : Tuer un panneau (panel)                     #|     |# ctrl +q : Pour quitter                            #|"
+echo "ctrl + a     | v            : Split vertical (split-window -h)            #|     |#  /      : Rechercher une chaine dans              #|"
+echo "ctrl + a     | s            : Split horizontal (split-window -v)          #|     |#  f      : Filtrer les lignes contenant une valeur #|"  
+echo "ctrl + a     | m            : Ouvrir une page de manuel dans une fenêtre  #|     |#  F      : Supprimer les filtres actifs            #|"  
+echo "ctrl + a     | Entrée       : Mode copie                                  #|     |# Shift+S : Statistiques de la colonne              #|"
+echo "ctrl + a     | k            : Confirmer avant de tuer une fenêtre         #|     |# Shift+G : Grouper les valeurs uniques de la col   #|"  
+echo "ctrl + a     | K            : Confirmer avant de tuer la session tmux     #|     |#    =    : Créer une colonne avec formule          #|" 
+echo "ctrl + a     | →            : Aller à la fenêtre suivante                 #|     |#    %    : Convertir col texte -> nombre           #|"
+echo "ctrl + a     | ←            : Aller à la fenêtre précédente               #|     |#  t/T    : Tri croissant/décroissant sur la col    #|"
+echo "ctrl + a     | ctrl + a     : Aller à la dernière fenêtre ouverte         #|     |#   u     : Déselectionner tout                     #|"
+echo "ctrl + a     | y            : Copier dans le presse-papiers (via plugin)  #|     |#   w     : Sauver les données selectionnées        #|"
+echo "ctrl+ a + N  |              : Switch d'une fenetre à l'autre              #|     |# ctrl+s  : Sauver toutes les feuilles              #|"
+echo "ctrl + a     | ls           : Afficher les sessions en cours              #|     |# ctrl+b  : Retour à la feuille précedente          #|"
+echo "------------------------------------------------------------------------  #|     |#####################################################|"
+echo "tmux kill-session           : revenir à propre                            #|"
+echo "                                                                          #|"
+echo "###########################################################################|" 
+echo "                          Raccourcis Bash Memo                            #|"
+echo "###########################################################################|"
+echo "                                                                          #|"
+echo "ln -s Cible Name            : Création d'un lien symbolique (rep/script)  #|"
+echo "killall ssh                 : Dégager les clients ssh si freeze           #|"
+
+
+
+
